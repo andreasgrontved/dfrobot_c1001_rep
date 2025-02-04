3d111319fe95b11f1720f4ece25447646dfe5918
andreasgrontved <144485801+andreasgrontved@users.noreply.github.com>
2025-02-04 23:42:05 +0100

    update

--- components/dfrobot_c1001/dfrobot_c1001_component.h
+++ components/dfrobot_c1001/dfrobot_c1001_component.h
@@ -8,22 +8,30 @@ namespace esphome {
 namespace dfrobot_c1001 {
 
 class DFRobotC1001Component : public PollingComponent, public uart::UARTDevice {
-public:
-    DFRobotC1001Component(UARTComponent *uart) : uart::UARTDevice(uart) {}
+ public:
+  // Constructor: Pass the UART component (which acts as a Stream) to the base.
+  DFRobotC1001Component(uart::UARTComponent *uart) : uart::UARTDevice(uart) {}
 
-    void setup() override;
-    void update() override;
-    void loop() override;  // Handle UART communication
+  void setup() override;
+  void update() override;
+  void loop() override;  // Called continuously to process UART data
 
-    // Sensor entities exposed to ESPHome
-    esphome::sensor::Sensor *human_presence_sensor{nullptr};
-    esphome::sensor::Sensor *human_movement_sensor{nullptr};
-    esphome::sensor::Sensor *fall_state_sensor{nullptr};
-    esphome::sensor::Sensor *residency_state_sensor{nullptr};
+  // Sensor pointers that will be set via configuration.
+  sensor::Sensor *human_presence_sensor{nullptr};
+  sensor::Sensor *human_movement_sensor{nullptr};
+  sensor::Sensor *fall_state_sensor{nullptr};
+  sensor::Sensor *residency_state_sensor{nullptr};
 
-private:
-    DFRobot_HumanDetection sensor_;  
-    uint32_t last_read_time_ = 0;  // Track last read time
+  // Setter methods called by code-generation.
+  void set_human_presence(sensor::Sensor *sensor) { human_presence_sensor = sensor; }
+  void set_human_movement(sensor::Sensor *sensor) { human_movement_sensor = sensor; }
+  void set_fall_state(sensor::Sensor *sensor) { fall_state_sensor = sensor; }
+  void set_residency_state(sensor::Sensor *sensor) { residency_state_sensor = sensor; }
+
+ private:
+  // The sensor object from the DFRobot_HumanDetection library.
+  DFRobot_HumanDetection sensor_;
+  uint32_t last_read_time_{0};
 };
 
 }  // namespace dfrobot_c1001
